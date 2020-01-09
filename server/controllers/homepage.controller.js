83b0d94d5ee67c17668d774d23242a501f39e836
Patryk Jarski <patryk.jarski@grandparade.co.uk>
2020-01-09 13:02:27 +0100

    Slider

--- server/controllers/homepage.controller.js
+++ server/controllers/homepage.controller.js
@@ -1,8 +1,22 @@
 const {OK} = require('http-status-codes');
+const news = [
+    {
+        heading: 'Billie Eilish woman of the Year!',
+        image: '/img/billie.jpg',
+        link: '/article/1',
+        source: 'BILLBOARD'
+    },
+    {
+        heading: `JLo's fab return with Jungle Dress!`,
+        image: '/img/jlo-getty.jpg',
+        link: '/article/2',
+        source: 'BILLBOARD'
+    },
+];
 
 const renderHomepage = (req, res, next) => {
     try {
-        res.status(OK).render('homepage/index');
+        res.status(OK).render('homepage/index', { news });
     } catch (e) {
         next(e);
     }
